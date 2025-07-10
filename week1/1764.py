a5ea4c1f8a3cfe522d07928bd20845503c3a3df0
carolyn <neige040515@gmail.com>
2025-07-10 12:34:00 +0900

    add 9375

--- week1/1764.py
+++ week1/1764.py
@@ -5,7 +5,7 @@ n, m = map(int, input().split())
 li = []
 
 for _ in range(n):
-    li.append(input().strip())  # strip()으로 개행 제거
+    li.append(input().strip()) 
 
 for _ in range(m):
     li.append(input().strip())
@@ -15,7 +15,7 @@ from collections import Counter
 li_counter = Counter(li)
 
 ans = [name for name, cnt in li_counter.items() if cnt == 2]
-ans.sort()  # 사전순 정렬
+ans.sort()
 
 print(len(ans))
 print("\n".join(ans))
